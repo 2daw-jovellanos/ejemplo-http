b6d18ab155f65f528dcfd454559e41f40886a41b
Victor fernandez <victor.fernandezgil@educa.madrid.org>
2018-11-21 17:39:51 +0100

    Inicial

---
+++ index.js
@@ -0,0 +1,13 @@
+var soyUnaTetera = false;
+
+function comprobarSiSoyUnaTetera() {
+    let soyUnaVariable = true;
+    if (soyUnaVariable === soyUnaTetera) {
+        alert("BlepBlepBlepBlepBlepBlep");
+    }
+    var leeme = 'Este es un código de ejemplo'
+            + 'que no hace nada'
+            + 'Para que lo depures';
+}
+
+comprobarSiSoyUnaTetera();
---
+++ tercera.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Tercera página</h1>
+<h2>Se reciben datos por post</h2>
+Escribiste "<?=$_REQUEST["algo"]?>"<br>
+<a href="index.html">Ir a primera página</a><br>
+<a href="segunda.html">Ir a segunda página</a><br>
+<script src="index.js"></script>
+</body>
+</html>
